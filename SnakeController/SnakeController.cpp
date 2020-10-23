2021de3e75ccf44cecafe0ebd9e760e925e85209
Grzegorz Domeradzki <grzegorz.domeradzki@nokia.com>
2020-10-23 16:17:25 +0200

    Bug Fix

--- SnakeController/SnakeController.cpp
+++ SnakeController/SnakeController.cpp
@@ -191,7 +191,7 @@ void Controller::handleDirectionInd(std::unique_ptr<Event> e)
 
 void Controller::updateFoodPosition(int x, int y, std::function<void()> clearPolicy)
 {
-    if (isSegmentAtPosition(x, y)) {
+    if (isSegmentAtPosition(x, y) || isPositionOutsideMap(x,y)) {
         m_foodPort.send(std::make_unique<EventT<FoodReq>>());
         return;
     }
